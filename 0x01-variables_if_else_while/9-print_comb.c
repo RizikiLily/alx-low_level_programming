b386ce33fb0d554ce344871b64d1b0f23128949b
RizikiLily <lillianriziki@gmail.com>
2023-06-16 13:06:33 -0700

    ./9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints out the last digit of a number
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
