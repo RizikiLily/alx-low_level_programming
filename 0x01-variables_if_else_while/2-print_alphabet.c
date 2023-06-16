2e3f1011ad49f8af1e359e3b6203a1fce0f761e9
RizikiLily <lillianriziki@gmail.com>
2023-06-16 11:28:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints out letters of the alphabet
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		++alpha;
+	}
+	putchar('\n');
+	return (0);
+}
