869b5a54db56ad6b4719d41933855712a0356908
RizikiLily <lillianriziki@gmail.com>
2023-06-16 15:09:02 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints out the last digit of a number
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 8 && j == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
