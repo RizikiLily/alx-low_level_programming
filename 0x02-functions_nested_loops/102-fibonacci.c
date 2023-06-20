33b672ba6f627e3ea104134af263bde40e67ce70
RizikiLily <lillianriziki@gmail.com>
2023-06-20 12:28:44 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - computes fibonacci numbers
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i = 0;
+	int first = 1;
+	int second = 2;
+	int next;
+
+	printf("%d, %d, ", first, second);
+	while (i != 48)
+	{
+		next = first + second;
+		if (i == 48)
+			printf("%d", next);
+		else
+			printf("%d, ", next);
+		first = second;
+		second = next;
+		i++;
+	}
+	return (0);
+}
