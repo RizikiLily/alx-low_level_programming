5d5d529cd3c4c7a7f646237a995bacb31245904f
RizikiLily <lillianriziki@gmail.com>
2023-06-20 09:16:44 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints evry minute of the day of jack_bauer
+ * Return: ...
+ */
+
+void jack_bauer(void)
+{
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 3; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(58);
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
