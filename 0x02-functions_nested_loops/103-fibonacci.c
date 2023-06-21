d5ef0aa62a8c58940183856027dcd4229f0245a2
RizikiLily <lillianriziki@gmail.com>
2023-06-21 04:06:48 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - computes fibonacci numbers with sum less than 4,000,000
+ * Return: 0(success)
+ */
+int main(void)
+{
+	long first;
+	long second;
+	long next;
+	long sum = 2;
+
+	while (next < 4000000)
+	{
+		next = first + second;
+		if  (next % 2 == 0)
+		{
+			sum += next;
+		}
+		first = second;
+		second = next;
+	}
+	printf("%ld", sum);
+	return (0);
+}
