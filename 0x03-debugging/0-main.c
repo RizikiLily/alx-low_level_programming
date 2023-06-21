cbcc1f5342edcbe574eb5444995072d1902b5598
RizikiLily <lillianriziki@gmail.com>
2023-06-21 11:39:18 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the function that checks + or - numbers
+ * Return: 0(succes)
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
