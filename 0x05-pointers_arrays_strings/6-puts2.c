0876385b5ef0e075a4b79a0b3e7713557b1f99d0
RizikiLily <lillianriziki@gmail.com>
2023-06-26 10:31:23 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @s: character to be modified
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (*str++)
+		len++;
+	for (i = 0; i < len; i += 2)
+	{
+	_putchar(str[i]);
+	}
+_putchar('\n');
+}
