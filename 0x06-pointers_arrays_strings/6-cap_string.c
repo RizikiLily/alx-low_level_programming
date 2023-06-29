fc1f2088362b922eff27f39b36c953e59fc3ade0
RizikiLily <lillianriziki@gmail.com>
2023-06-29 03:52:38 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - converts strings to uppercase
+ * @str: strind to be converted
+ * Return: Converted string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
+			str[i + 1] = str[i + 1] + 'A' - 'a';
+	}
+	return (str);
+}
