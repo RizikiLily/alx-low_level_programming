51a4fbf6055e20803af438326f8546a003160155
RizikiLily <lillianriziki@gmail.com>
2023-06-29 04:44:20 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: characters to be encoded
+ * Return: encoded character
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
