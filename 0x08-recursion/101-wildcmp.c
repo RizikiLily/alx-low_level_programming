37caf9baaa6f979576071a08ff75a5f70771f605
RizikiLily <lillianriziki@gmail.com>
2023-07-05 14:41:30 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * wildcmp - Compare strings
- * @s1: pointer to string params
- * @s2: pointer to string params
+ * @s1: pointer to string
+ * @s2: pointer to string
  * Return: 0
  */
 int wildcmp(char *s1, char *s2)
