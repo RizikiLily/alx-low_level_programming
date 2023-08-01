bc3a9b64e42b86384443ab59052d05add85b097a
RizikiLily <lillianriziki@gmail.com>
2023-08-01 00:08:50 -0700

    print the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			temp = temp->next;
 			i++;
 			if (!temp)
-				return (NULL);
+				return (temp ? temp : NULL);
 		}
 	}
 	return (temp);
