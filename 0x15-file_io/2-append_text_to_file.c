7097dd7bc4aff48e66683d20179e05ca55f3be08
RizikiLily <lillianriziki@gmail.com>
2023-08-07 14:33:40 -0700

    copy from one file to another

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,26 +9,26 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fp, i, w;
+
 	int len = 0;
-	
+
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	
 	if (text_content == NULL)
 		return (-1);
-	else
-		for (i = 0; text_content[i] != '\0'; i++)
-		{
-			len++;
-		}
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+		len++;
+	}
 	fp = open(filename, O_WRONLY | O_APPEND, 664);
 	w = write(fp, text_content, len);
 	if (fp == -1 || w == -1)
 	{
 		return (-1);
 	}
-	close (fp);
+	close(fp);
 	return (1);
 }
